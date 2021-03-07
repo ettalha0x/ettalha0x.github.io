1c2dedca57e1283d8024b1d8f1b9f5f618d0574c
Noureddine Ettalhaouy <31565378+Noureddine-Ettalhaouy@users.noreply.github.com>
2021-03-07 23:56:06 +0100

    Create my portfolio project

---
+++ VueJs/info.js
@@ -0,0 +1,27 @@
+    var Profile = new Vue({
+        el : '#Profile',
+        data : {
+            image : 'img/me.jpeg',
+            title : 'Noureddine Ettalhaouy',
+            job : 'Front-End Developer',
+        },
+    })
+    var ProfileInfo = new Vue({
+        el : '#ProfileInfo',
+        data : {
+            fullName : 'Noureddine Ettalhaouy',
+            email : 'Noureddine Ettalhaouy',
+            phone : 'Front-End Developer',
+            address : 'Center Ait Daoud 44060 Essaouira',
+        },
+    })
+    var SocialNetwork = new Vue({
+        el : '#SocialNetwork',
+        data : {
+            webSite : 'nourtech.com',
+            gitHub : 'Noureddine-Ettalhaouy',
+            facebook : 'Noureddine Ettalhaouy',
+            instagram : 'noureddine_ettalhaouy',
+            twitter : 'Ettalhaoui151201',
+        }
+    })
