057c002bdaf1edd941a5168600d90eaba4911c9c
Noureddine Ettalhaouy <nettalha@c2r7p8.1337.ma>
2023-08-05 19:27:19 +0100

    upadte

--- VueJs/info.js
+++ VueJs/info.js
@@ -1,1 +1,8 @@
-    
+    var Profile = new Vue({
+        el : '#profileInfo',
+        data : {
+            image : 'img/31565378.png',
+            title : 'Noureddine Ettalhaouy',
+            job : 'Software engineer student at 1337',
+        },
+    })
