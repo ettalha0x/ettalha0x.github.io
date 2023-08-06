a6ab03f14c2358a2ac0b4d6f708c289166ccff47
Noureddine Ettalhaouy <31565378+ettalha0x@users.noreply.github.com>
2023-08-06 13:11:46 +0100

    Update info.js

--- VueJs/info.js
+++ VueJs/info.js
@@ -1,7 +1,7 @@
     var Profile = new Vue({
         el : '#profileInfo',
         data : {
-            image : 'img/31565378.png',
+            image : 'img/pfp.png',
             title : 'Noureddine Ettalhaouy',
             job : 'Software engineer student at 1337',
         },
