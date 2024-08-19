c4dc899b0cd82a39350f53975dc5ef4f667744a9
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2024-08-19 17:21:48 +0100

    update portfolio

--- VueJs/info.js
+++
@@ -1,8 +0,0 @@
-    var Profile = new Vue({
-        el : '#profileInfo',
-        data : {
-            image : 'img/pfp.png',
-            title : 'Noureddine Ettalhaouy',
-            job : 'Software engineer student at 1337',
-        },
-    })
